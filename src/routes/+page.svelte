c7cb1ed86c9afcac37ebdc0212eab56c203c68d3
stygigoth <99850233+stygigoth@users.noreply.github.com>
2022-09-20 21:07:23 -0400

    typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to Prometheus Studios</h1>
 <h3><br>A game studio dedicated to providing the highest quality voxel games</h3>
-<h3><br><a href="https://discord.gg/AJZUPdB6XK">Join us on Discord too keep up with development!</a></h3>
+<h3><br><a href="https://discord.gg/AJZUPdB6XK">Join us on Discord to keep up with development!</a></h3>
