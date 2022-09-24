79802649370f2db4da42e4fc8ea0b0b72081bc96
stygigoth <nyxwilliams@protonmail.com>
2022-09-24 09:46:11 -0400

    mobile support

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,10 +1,28 @@
 <nav class="header">
     <a href="/" class="logo"><img src="/favicon.png" style="height: 80px" alt=""></a>
     <div class="nav-links">
-        <p><a href="/">Home</a></p>
+        <p class="hide"><a href="/">Home</a></p>
         <p><a href="/blog">Blog</a></p>
         <p><a href="/prometheus">Prometheus</a></p>
         <p><a href="/kindling">Kindling</a></p>
         <p><a href="/firestarter">Firestarter</a></p>
     </div>
 </nav>
+
+<style>
+    p {
+        float: right;
+        padding-right: 2vw;
+    }
+        
+    .nav-links {
+        position: absolute;
+        right: 0;
+    }
+        
+    @media only screen and (max-device-width: 415px) {
+        .hide {
+            display: none;
+        }
+    }
+</style>
